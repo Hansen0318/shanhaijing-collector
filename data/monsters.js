e45a7fbdbcce62e9121166fc6cd58c24a8a80392
Hansen0318 <lovehandsomeman@gmail.com>
2026-08-14 22:51:54 +0800

    Add files via upload

--- data/monsters.js
+++ data/monsters.js
@@ -1,8 +1,1 @@
-// 第一階段只提供場景中的未發現妖獸線索。
-export const monsters = [{
-  id: "nine-tailed-fox",
-  name: "九尾狐",
-  region: "青丘",
-  hint: "月色落下時，狐火會在古樹旁閃動。",
-  position: { x: 1160, y: 650 }
-}];
+Unsupported Media Type
